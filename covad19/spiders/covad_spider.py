d37e19133ac93271f26bf40fda13d435dc33f25b
SeriousHyena <50051489+SeriousHyena@users.noreply.github.com>
2020-03-09 15:06:27 -0700

    incremental

---
+++ covad19/spiders/covad_spider.py
@@ -0,0 +1,12 @@
+# the github repo for project is:
+# https://github.com/SeriousHyena/covad-19-scrape/blob/master/covad19/spiders/covad_spider.py
+
+import scrapy
+from ..items import Covad19Item
+
+class CovadSpider(scrapy.Spider):
+    name = 'covad'
+    start_urls = ['https://www.worldometers.info/coronavirus/']
+
+    def parse(self, response):
+        all_countries = response.css('a.mt_a a::(href)').extract
